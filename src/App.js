0a0d55ec80329fc0da21b2f8476bd430ea5b52cd
dinukrana2000 <dinuk2000rana@gmail.com>
2024-01-24 19:49:38 +0530

    add route for home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Home from './Pages/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <Home/>
+     <BrowserRouter>
+     <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }

cd183aebb371313b07578818b74deec96240f57b
dinukrana2000 <dinuk2000rana@gmail.com>
2024-01-25 22:24:53 +0530

    initial commit

---
+++ src/Pages/UserRequestHome/UserReq.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavigationBar from '../../Components/Navbar/NavigationBar'
+function UserReq() {
+  return (
+    <div>
+        <NavigationBar />
+        
+    </div>
+  )
+}
+
+export default UserReq
